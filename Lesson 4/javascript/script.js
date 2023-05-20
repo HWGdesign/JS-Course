a1951f12d764576c8b2642be7f43b5182443cb02
HWG - Jakub Hewig <hwgpraca@gmail.com>
2023-05-20 20:09:25 +0200

    lesson 4

---
+++ Lesson 4/javascript/script.js
@@ -0,0 +1,17 @@
+const txt = 'some text';
+const score = 12.55;
+console.log(txt.length);
+console.log(txt[2]);
+console.log(txt.toUpperCase());
+
+console.log(score.toFixed() );
+console.log(score.toFixed(1) );
+console.log(txt.indexOf('e') );
+
+const name = 'Jacob';
+console.log(name.length);
+
+const userName = new String('RandomName');
+const userID = new Number(1);
+const isMale = new Boolean(true);
+console.log(userName, userID, isMale); //make instance
