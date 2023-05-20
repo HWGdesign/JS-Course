00c103b2c1a5a4eae91a668fa8e7f69b74d35c84
HWG - Jakub Hewig <hwgpraca@gmail.com>
2023-05-20 19:44:48 +0200

    JS

--- Lesson 3/javascript/script.js
+++ Lesson 3/javascript/script.js
@@ -1,8 +1,34 @@
+console.log('__ASIGN, COMPARE__');
+
 let number;
 console.log(number);
-number = 3;
-console.log('2' == 2);
-console.log('2' === 2);
-
+number = 3; //asign value
+console.log(number);
+console.log('2' == 2); //comparison with type conversion
+console.log('2' === 2); //comparison without type conversion
 console.log(100 == 101);
 console.log(100 != 101);
+console.log(100 !== 100);
+console.log('100' != 100);
+console.log('100' !== 100);
+
+console.log('___NEGATION___');
+
+console.log(!''); // negation changes the type to boolean and inverts
+console.log(!'kuba'); 
+console.log(!!''); // double-negation changes the type to boolean and double-inverts
+console.log(!!'kuba'); 
+
+console.log('__OR__');
+
+console.log( 2 || 5); // OR
+console.log(0 || true);
+
+console.log('__AND__');
+
+console.log(2 && 1); // AND
+console.log(2 && 0);
+
+console.log('__TRIPLE__');
+
+console.log(3 > 2 ? "tak" : "nie"); //IF YES -> PRINT THING ON LEFT, IF NOT -> PRINT THING ON RIGHT
