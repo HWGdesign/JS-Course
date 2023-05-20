bb1493ccac8691c3648744106f13ab4ad4cc9de0
HWG - Jakub Hewig <hwgpraca@gmail.com>
2023-05-20 17:29:09 +0200

    changes

---
+++ Lesson 2/javascript/script.js
@@ -0,0 +1,22 @@
+let firstLetterUserName;
+let userID = 1;
+
+console.log(userID);
+
+console.log(firstLetterUserName);
+
+firstLetterUserName = 'J';
+console.log(firstLetterUserName);
+
+let showID = function() {
+     console.log(userID)
+};
+
+showID();
+
+let position = [200,100], score = null, time = 20.2, ipAddress;
+
+let exampleOne = 200;
+exampleOne++;
+
+console.log(exampleOne);
