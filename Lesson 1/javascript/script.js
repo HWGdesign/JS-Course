0fca4c4ecdf638258fa66974e7ce8593952af065
HWG - Jakub Hewig <hwgpraca@gmail.com>
2023-05-20 15:24:48 +0200

    JS course

---
+++ Lesson 1/javascript/script.js
@@ -0,0 +1,12 @@
+console.log('ok');
+
+let result = 2 + 2;
+console.log(result);
+
+let resultTwo = 'aaa';
+console.log(resultTwo);
+
+const usersPoints = [1,2,3,4];
+let points = 10;
+
+console.log(usersPoints);
