686eef495335d000ae7b84f1db3388bcd10b8a0b
HWG - Jakub Hewig <hwgpraca@gmail.com>
2023-05-21 00:05:35 +0200

    lesson 5

--- Lesson 4/javascript/script.js
+++ Lesson 4/javascript/script.js
@@ -15,3 +15,19 @@ const userName = new String('RandomName');
 const userID = new Number(1);
 const isMale = new Boolean(true);
 console.log(userName, userID, isMale); //make instance
+
+//Task 
+
+let myVariable = 10;
+let myName = 'Benedykt';
+
+console.log(typeof(myVariable));
+console.log(typeof(myName));
+myVariable = 11;
+myName = 'Mieszko';
+
+let sum = myVariable + 5;
+let greeting = myName + " witaj w świecie Javascript"
+
+console.log(sum);
+console.log(greeting);
